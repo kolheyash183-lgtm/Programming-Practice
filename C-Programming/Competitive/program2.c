e84a72f43c6f5e5a50be11f7b4112057fcd89f3a
Yash Kolhe <kolheyash183@gmail.com>
2026-06-03 13:34:00 +0530

    Create program2.c

---
+++ C-Programming/Competitive/program2.c
@@ -0,0 +1,17 @@
+// program to print 5 items "Marvellous"on screen
+
+#include<stdio.h>
+
+void Display()
+{
+    int i =0;
+    for(i=1;i<=5;i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
