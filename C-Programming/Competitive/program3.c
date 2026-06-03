d39d11cfc88eb0c88134d49e19558079c24eeccb
Yash Kolhe <kolheyash183@gmail.com>
2026-06-03 13:34:38 +0530

    Create program3.c

---
+++ C-Programming/Competitive/program3.c
@@ -0,0 +1,19 @@
+//program to print 5 to 1 numbers on screen
+#include<stdio.h>
+void Display()
+{
+    int i=0;
+    int i = 5;
+    while(i>=1)
+    {
+        printf("%d",i);
+        i--;
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+    
+
+}
