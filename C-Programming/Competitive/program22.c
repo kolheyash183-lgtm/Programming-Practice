c280e633f1066728e3259234df692c79f8587b7f
Yash Kolhe <kolheyash183@gmail.com>
2026-06-12 14:26:08 +0530

    Create program22.c

---
+++ C-Programming/Competitive/program22.c
@@ -0,0 +1,26 @@
+// 2. Print numbers till N
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int i = 0;
+
+    for(i = 1; i <= iNo; i++)
+    {
+        printf("%d\t",i);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number 
+        ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
