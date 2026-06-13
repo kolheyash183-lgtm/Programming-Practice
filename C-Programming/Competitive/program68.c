9896a0b15613eb4b7bdc1b7aeff86b8fbdc8d7cd
Yash Kolhe <kolheyash183@gmail.com>
2026-06-13 14:15:23 +0530

    Create program68.c

---
+++ C-Programming/Competitive/program68.c
@@ -0,0 +1,62 @@
+//Accept N numbers from user check whether that numbers contains 11 in it or not.
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL Check(int Arr[], int iLength)
+{
+    int i = 0;
+
+    for(i = 0; i < iLength; i++)
+    {
+        if(Arr[i] == 11)
+        {
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+int main()
+{
+    int iSize = 0, iCnt = 0;
+    int *p = NULL;
+    BOOL bRet = FALSE;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter elements\n");
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+
+    bRet = Check(p,iSize);
+
+    if(bRet == TRUE)
+    {
+        printf("11 is present");
+    }
+    else
+    {
+        printf("11 is absent");
+    }
+
+    free(p);
+
+    return 0;
+}
