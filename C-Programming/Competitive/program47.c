697899877c8f1f668524af38cf1a6e655230c865
Yash Kolhe <kolheyash183@gmail.com>
2026-06-13 14:01:07 +0530

    Create program47.c

---
+++ C-Programming/Competitive/program47.c
@@ -0,0 +1,30 @@
+//Write a program which accept width & height of rectangale from user and calculate its area (Area = width * Height)
+
+////Input : 5.3 9.78
+//Output : 51.834
+
+#include<stdio.h>
+double ReactArea(float fWidth, float fHeight)
+{
+    return fWidth * fHeight;
+}
+
+int main()
+
+{
+    float fValue1 = 0.0, fValue2 = 0.0;
+    double dRet = 0.0;
+
+    printf("Enter width");
+    scanf("%f",&fValue1);
+     
+    printf("Enter  height");
+    scanf("%f",&fValue2);
+
+    dRet = RectArea(fValue1,fValue2);
+
+    printf("Area of width is : &f\n",dRet);
+
+    return 0;
+
+}
