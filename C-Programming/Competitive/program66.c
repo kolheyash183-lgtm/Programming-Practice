e89866c56928f515c741306123521930a9f01846
Yash Kolhe <kolheyash183@gmail.com>
2026-06-13 14:14:05 +0530

    Create program66.c

---
+++ C-Programming/Competitive/program66.c
@@ -0,0 +1,48 @@
+//Accept N numbers from user and return freqency of even number
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountEven(int Arr[], int iLength)
+{
+    int iCnt = 0, i = 0;
+
+    for(i = 0; i < iLength; i++)
+    {
+        if(Arr[i] % 2 == 0)
+        {
+            iCnt++;
+        }
+    }
+    return iCnt;
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter elements\n");
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = CountEven(p,iSize);
+
+    printf("Result is %d",iRet);
+
+    free(p);
+
+    return 0;
+}
