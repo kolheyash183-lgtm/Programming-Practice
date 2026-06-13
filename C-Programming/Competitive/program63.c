9d8a07c19c32e86b0b7cbf68293f53b9a28cd188
Yash Kolhe <kolheyash183@gmail.com>
2026-06-13 14:11:52 +0530

    Create program63.c

---
+++ C-Programming/Competitive/program63.c
@@ -0,0 +1,47 @@
+//Accept n numbers from user and display all such elemnts which are even and divisible by 5
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display(int Arr[], int Length)
+{
+    int i = 0;
+
+    for(i = 0; i < Length; i++)
+    {
+        if((Arr[i] % 2 == 0) && (Arr[i] % 5 == 0))
+        {
+            printf("%d\t",Arr[i]);
+        }
+    }
+}
+
+int main()
+{
+    int Size = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&Size);
+
+    p = (int *)malloc(Size * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements\n",Size);
+
+    for(iCnt = 0; iCnt < Size; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    Display(p, Size);
+
+    free(p);
+
+    return 0;
+}
