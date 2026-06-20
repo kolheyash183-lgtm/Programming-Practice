75a848ce1f7fb281a457f4ca4012412c0de7e0e7
Yash Kolhe <kolheyash183@gmail.com>
2026-06-20 18:31:29 +0530

    Create program109.c

---
+++ C-Programming/Competitive/program109.c
@@ -0,0 +1,25 @@
+//Accept number from user and display below pattern
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    for(int i = 1; i <= iNo; i++)
+    {
+        printf("*\t%d\t", i);
+    }
+
+    printf("*");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
