4b69b97f30066d8c74e2e587a36a76bf9dc75b71
Yash Kolhe <kolheyash183@gmail.com>
2026-06-20 17:41:16 +0530

    Create program99.java

---
+++ Java-Programming/Competitive/program99.java
@@ -0,0 +1,20 @@
+//Write a program to find the largest digit in a given numbers
+
+class Logic{
+    void findLargestDigit(int num)
+    {
+
+    }
+}
+
+public class program99
+{
+    public static void main(String[] args) 
+    
+    {
+        Logic obj = new Logic();
+        obj.findLargestDigit(83429);
+        
+    }
+    
+}
