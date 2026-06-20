1f0ef8062785a7a7ebb94934c04bc66e0816aa76
Yash Kolhe <kolheyash183@gmail.com>
2026-06-20 17:41:53 +0530

    Create program100.java

---
+++ Java-Programming/Competitive/program100.java
@@ -0,0 +1,18 @@
+// write  a prgram to find the smallest digit in agiven number
+class Logic{
+    void findSmallestDigit(int num)
+    {
+        //logic
+    }
+}
+
+class program100
+{
+    public static void main(String[] args)
+    {
+       Logic obj = new Logic();
+       obj.findSmallestDigit(45872);
+        
+    }
+    
+}
