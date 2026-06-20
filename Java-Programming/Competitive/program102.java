5dd9da9e976d0f792ccef0fe97b9f69b21b691aa
Yash Kolhe <kolheyash183@gmail.com>
2026-06-20 18:03:49 +0530

    Create program102.java

---
+++ Java-Programming/Competitive/program102.java
@@ -0,0 +1,32 @@
+//count even and odd numbers between 1 and n
+class Logic
+{
+    void countEvenOddRange(int n)
+    {
+        int even = 0, odd = 0;
+
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+
+        System.out.println("Even Count = " + even);
+        System.out.println("Odd Count = " + odd);
+    }
+}
+
+class Program102
+{
+    public static void main(String args[])
+    {
+        Logic obj = new Logic();
+        obj.countEvenOddRange(50);
+    }
+}
