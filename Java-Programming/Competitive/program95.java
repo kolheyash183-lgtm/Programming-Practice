f4874848f93109e510a75203cf3f43ce0a702ccf
Yash Kolhe <kolheyash183@gmail.com>
2026-06-20 17:38:14 +0530

    Create program95.java

---
+++ Java-Programming/Competitive/program95.java
@@ -0,0 +1,26 @@
+//Write a program to calculate the power of a number using loops
+// Write a program to calculate the power of a number using loops
+
+class Logic
+{
+    void calculatePower(int base, int exp)
+    {
+        int result = 1;
+
+        for(int i = 1; i <= exp; i++)
+        {
+            result = result * base;
+        }
+
+        System.out.println("Power is : " + result);
+    }
+}
+
+class program95
+{
+    public static void main(String[] args)
+    {
+        Logic obj = new Logic();
+        obj.calculatePower(2, 5);
+    }
+}
