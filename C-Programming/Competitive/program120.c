56c9a20125bed5ebb083cc7bb64d1268e818dfae
Yash Kolhe <kolheyash183@gmail.com>
2026-06-25 09:42:56 +0530

    Create program120.c

---
+++ C-Programming/Competitive/program120.c
@@ -0,0 +1,29 @@
+//Accwpt number of rows and number of columns from user and display below pattern 
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    int iCnt = 1;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("%d\t", iCnt);
+            iCnt++;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
