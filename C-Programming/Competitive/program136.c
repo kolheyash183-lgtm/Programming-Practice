642daf80e54b4b8e11cfbc90a52f89507a0d4a09
Yash Kolhe <kolheyash183@gmail.com>
2026-06-26 12:43:56 +0530

    Create program136.c

---
+++ C-Programming/Competitive/program136.c
@@ -0,0 +1,35 @@
+//Accept number of rows and columns from user and display below pattern
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i,j;
+
+    for(i=1;i<=iRow;i++)
+    {
+        for(j=1;j<=iCol;j++)
+        {
+            if(i+j == iCol+1)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter rows and columns : ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
