7953ec0fbbb9580e2e8364a952a66e8dbefe1f98
Yash Kolhe <kolheyash183@gmail.com>
2026-07-02 10:52:20 +0530

    Create program153.c

---
+++ C-Programming/Competitive/program153.c
@@ -0,0 +1,39 @@
+//Differnce Between Small And Capital Characters
+#include<stdio.h>
+
+int Difference(char *str)
+{
+    int iSmall = 0;
+    int iCapital = 0;
+
+    while(*str != '\0')
+    {
+        if(*str >= 'a' && *str <= 'z')
+        {
+            iSmall++;
+        }
+        else if(*str >= 'A' && *str <= 'Z')
+        {
+            iCapital++;
+        }
+
+        str++;
+    }
+
+    return (iSmall - iCapital);
+}
+
+int main()
+{
+    char arr[50];
+    int iRet = 0;
+
+    printf("Enter string : ");
+    scanf("%[^'\n']s",arr);
+
+    iRet = Difference(arr);
+
+    printf("Difference is : %d",iRet);
+
+    return 0;
+}
