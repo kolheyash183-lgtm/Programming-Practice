0f4866d5c5944325f61d8f708233a8dce9e4c6ab
Yash Kolhe <kolheyash183@gmail.com>
2026-07-02 10:33:46 +0530

    Create program145.c

---
+++ C-Programming/Competitive/program145.c
@@ -0,0 +1,39 @@
+//Display Exam Schedule according to Division (Case Insensitive)
+
+#include<stdio.h>
+
+void DisplaySchedule(char chDiv)
+{
+    if(chDiv == 'A' || chDiv == 'a')
+    {
+        printf("Your exam at 7.00 AM");
+    }
+    else if(chDiv == 'B' || chDiv == 'b')
+    {
+        printf("Your exam at 8.30 AM");
+    }
+    else if(chDiv == 'C' || chDiv == 'c')
+    {
+        printf("Your exam at 9.20 AM");
+    }
+    else if(chDiv == 'D' || chDiv == 'd')
+    {
+        printf("Your exam at 10.30 AM");
+    }
+    else
+    {
+        printf("Invalid Division");
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+
+    printf("Enter your division : ");
+    scanf("%c",&cValue);
+
+    DisplaySchedule(cValue);
+
+    return 0;
+}
