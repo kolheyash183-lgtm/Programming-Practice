cc08f1016f6f3c8ff8a7e8a373246ed5cb9e59a1
Yash Kolhe <kolheyash183@gmail.com>
2026-07-05 20:57:19 +0530

    Create program4.java

---
+++ Java-Programming/Practice/program4.java
@@ -0,0 +1,24 @@
+
+abstract class Demo
+{
+    public int i,j;
+
+    public int Add(int a, int b)
+    {
+        return 0;
+    }
+
+    public abstract int Sub(int a, int b);
+
+}
+
+class Hello extends Demo
+
+
+class abstract_demoX
+{
+    public static void main(String A[] )
+     {
+        Hello hobj = new Hello();
+    }
+}
