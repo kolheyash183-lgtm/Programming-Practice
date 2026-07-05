6fb9490c4881c4e631cd7d1d0c117405e8c9e008
Yash Kolhe <kolheyash183@gmail.com>
2026-07-05 21:55:50 +0530

    Create program168.c

---
+++ C-Programming/Competitive/program168.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void StrCpyCap(char *src, char *dest)
+{
+    while(*src != '\0')
+    {
+        if((*src >= 'A') && (*src <= 'Z'))
+        {
+            *dest = *src;
+            dest++;
+        }
+
+        src++;
+    }
+
+    *dest = '\0';
+}
+
+int main()
+{
+    char arr[30];
+    char brr[30];
+
+    printf("Enter string : ");
+    scanf("%[^'\n']s",arr);
+
+    StrCpyCap(arr,brr);
+
+    printf("Capital characters are : %s\n",brr);
+
+    return 0;
+}
