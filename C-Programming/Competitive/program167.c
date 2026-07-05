39bb83cef4347ffdce771d72037a064829596258
Yash Kolhe <kolheyash183@gmail.com>
2026-07-05 21:55:21 +0530

    Create program167.c

---
+++ C-Programming/Competitive/program167.c
@@ -0,0 +1,34 @@
+//Write a program to accept string from user  and copy the contaains of that strng into another string 
+ #include<stdio.h>
+
+void StrNCpyX(char *src, char *dest, int icnt)
+{
+    while((*src != '\0') && (icnt != 0))
+    {
+        *dest = *src;
+        src++;
+        dest++;
+        icnt--;
+    }
+
+    *dest = '\0';
+}
+
+int main()
+{
+    char arr[30];
+    char brr[30];
+    int iNo = 0;
+
+    printf("Enter string : ");
+    scanf("%[^'\n']s",arr);
+
+    printf("Enter number of characters : ");
+    scanf("%d",&iNo);
+
+    StrNCpyX(arr,brr,iNo);
+
+    printf("Copied string is : %s\n",brr);
+
+    return 0;
+}
