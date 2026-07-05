58c608e355687f94b71c8d29c501b8d1288a5ed8
Yash Kolhe <kolheyash183@gmail.com>
2026-07-05 20:55:08 +0530

    Create program2.java

---
+++ Java-Programming/Practice/program2.java
@@ -0,0 +1,20 @@
+
+abstract class Demo
+{
+    public int i,j;
+
+    public int Add(int a, int b)
+    {
+        return 0;
+    }
+
+}
+
+
+class abstract_demo
+{
+    public static void main(String A[] )
+     {
+        Demo dobj = new Demo();
+    }
+}
