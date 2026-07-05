cc36ed56f14ad50fe2519304bcf07d18d9d98bbe
Yash Kolhe <kolheyash183@gmail.com>
2026-07-05 21:41:08 +0530

    Create program162.c

---
+++ C-Programming/Competitive/program162.c
@@ -0,0 +1,39 @@
+//Write a program which accept string from user and accept one cheacter return frequency of that charcter
+#include<stdio.h>
+
+int CountChar(char *str, char ch)
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == ch)
+        {
+            iCnt++;
+        }
+        str++;
+    }
+
+    return iCnt;
+}
+
+int main()
+{
+    char arr[20];
+    char cValue;
+    int iRet = 0;
+
+    printf("Enter string : ");
+    scanf("%[^\n]s",arr);
+
+    getchar();
+
+    printf("Enter character : ");
+    scanf("%c",&cValue);
+
+    iRet = CountChar(arr,cValue);
+
+    printf("Character frequency is %d\n",iRet);
+
+    return 0;
+}
