b8ccbc527ee0fe2383bf5babceaad3cc17bc0cd8
Yash Kolhe <kolheyash183@gmail.com>
2026-07-07 14:30:20 +0530

    Create program178.c

---
+++ C-Programming/Competitive/program178.c
@@ -0,0 +1,25 @@
+//write a program which accept one number from user and toggle 7th bit of that number return modified number
+ #include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT ToggleBit(UINT iNo)
+{
+    UINT iMask = 0x40;
+
+    return (iNo ^ iMask);
+}
+
+int main()
+{
+    UINT iValue = 0, iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%u",&iValue);
+
+    iRet = ToggleBit(iValue);
+
+    printf("Modified number : %u\n",iRet);
+
+    return 0;
+}
