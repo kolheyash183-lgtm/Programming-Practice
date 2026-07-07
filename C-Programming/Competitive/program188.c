72371edbf79800b70064a24b017dc92ca7dddf3e
Yash Kolhe <kolheyash183@gmail.com>
2026-07-07 14:54:26 +0530

    Create program188.c

---
+++ C-Programming/Competitive/program188.c
@@ -0,0 +1,42 @@
+//Check Whether 9th or 12th Bit is On
+#include<stdio.h>
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkBit(UINT iNo)
+{
+    UINT iMask = 0x900;
+
+    if((iNo & iMask) != 0)
+    {
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : ");
+    scanf("%u",&iValue);
+
+    bRet = ChkBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("TRUE\n");
+    }
+    else
+    {
+        printf("FALSE\n");
+    }
+
+    return 0;
+}
