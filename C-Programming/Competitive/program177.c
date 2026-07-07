d06dd8a13036967dde0ebe7c2f477fec741dbf95
Yash Kolhe <kolheyash183@gmail.com>
2026-07-07 14:29:59 +0530

    Create program177.c

---
+++ C-Programming/Competitive/program177.c
@@ -0,0 +1,25 @@
+////Write a program which accept one number from user and off 7th and 10th bit of that number return modified number
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT OffBit(UINT iNo)
+{
+    UINT iMask = 0xFFFFFDBF;   // ~(0x240)
+
+    return (iNo & iMask);
+}
+
+int main()
+{
+    UINT iValue = 0, iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%u",&iValue);
+
+    iRet = OffBit(iValue);
+
+    printf("Modified number : %u\n",iRet);
+
+    return 0;
+}
