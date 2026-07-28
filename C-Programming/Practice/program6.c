c8e93776741784699fa600303bcf87f02d078c3c
Yash Kolhe <kolheyash183@gmail.com>
2026-07-28 17:23:38 +0530

    Create program6.c

---
+++ C-Programming/Practice/program6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    char cArr[4] ={'a','b','c','d'};
+    int iArr[4] ={10,20,30,40};
+    float fArr[4] ={10.0f,20.0f,30.0f,40.0f};
+    double dArr[4] ={10.0,20.0,30.0,40.0};
+    printf ("size of character array :%lu\n",sizeof(cArr));
+
+
+    return 0;
+}
